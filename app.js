b46acab6e3182b83f7147e5e4f327e0286df59f3
devesh48 <upadhyay.dev48@gmail.com>
2018-11-24 12:05:05 +0530

    adding middleware

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+var multer = require('multer');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,10 +19,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//File upload handler
+//app.use(multer({dest : './public/images'}).none);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Handle express session
+app.use(session({
+	secret : 'this is a secret',
+	saveUninitialized : true,
+	resave : true
+}));
+
+// Validator
+app.use(expressValidator({
+	errorFormatter : function(param, msg, value){
+		var namespace = param.split('.')
+		var root = namespace.shift();
+		var formParam = root;
+		
+		while (namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param : formParam,
+			msg : msg,
+			value : value
+		};
+	}
+}));
+
+// connect flash
+app.use(flash());
+app.use(function (req,res,next){
+	res.locals.messages = require('express-messages')(req,res);
+	next();
+});
+
+//make our db accessible to our router
+app.use(function(req,res,next){
+	req.db = db;
+	next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
